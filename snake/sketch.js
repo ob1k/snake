403292eee3c1f3fbc9f643cafc3e6c4361ecc291
ob1k <ob1k2@icloud.com>
2016-12-11 10:47:01 -0500

    uploading p5*js sketch

---
+++ snake/sketch.js
@@ -0,0 +1,62 @@
+var s;
+var scl = 20;
+
+var food;
+
+function setup() {
+  createCanvas(500,400);
+  s = new Snake();
+  frameRate(12);
+  pickLocation();
+
+}
+
+function pickLocation() {
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  food = createVector(floor(random(cols)), floor(random (rows)));
+  food.mult(scl);
+}
+
+function draw() {
+  background(51);
+  s.update();
+  s.show();
+  
+  if (s.eat(food)) {
+    pickLocation();
+  }
+
+  fill(255, 0, 100);
+  ellipse(food.x, food.y, scl, scl);
+}
+  
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);  
+  }
+}
+  
+function Snake() {
+  this.x = 0;
+  this.y =0;
+  this.xspeed = 1;
+  this.yspeed = 0;
+  
+  this.update = function() {
+    this.x = this.x + this.xspeed;
+    this.y = this.y + this.yspeed;
+  }
+  
+  this.show = function() {
+    fill(200);
+    ellipse(this.x, this.y, 10, 10);
+    
+  }
+}
